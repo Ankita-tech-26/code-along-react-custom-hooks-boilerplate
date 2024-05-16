fc6644e9102b6cfc0d27a2252bd1d1a8ae682be1
Ankita Laxmi <74234178+Ankita-tech-26@users.noreply.github.com>
2024-05-16 22:16:44 +0530

    Added

--- custom-hook/src/App.jsx
+++ custom-hook/src/App.jsx
@@ -2,18 +2,18 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 const useStorage = (key, initialValue) => {
-  // Retrieve the value from local storage, or use the initialValue
+  
   const [value, setValue] = useState(() => {
     const storedValue = localStorage.getItem(key);
     return storedValue !== null ? JSON.parse(storedValue) : initialValue;
   });
 
-  // Update local storage when the value changes
+  
   useEffect(() => {
     localStorage.setItem(key, JSON.stringify(value));
   }, [key, value]);
 
-  // Function to update the value
+  
   const updateValue = (newValue) => {
     setValue(newValue);
   };
@@ -21,7 +21,6 @@ const useStorage = (key, initialValue) => {
   return [value, updateValue];
 };
 
-// Usage example:
 const App = () => {
   const [inputValue, setInputValue] = useStorage('textInput', '');
 
